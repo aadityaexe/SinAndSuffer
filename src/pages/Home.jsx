0f1b344bb80c25a9bef2d90f85205a854e6d6bba
adityaexe <kmradi2002@gmail.com>
2025-07-13 10:47:14 +0530

    Add ScrollingText and Footer components to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,18 @@ import Navbar from "../components/Navbar";
 import About from "../components/About";
 import Hero from "../components/Hero";
 import Faq from "../components/Faq";
+import ScrollingText from "../components/ScrollingText";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <ScrollingText />
       <About />
       <Faq />
+      <Footer />
     </div>
   );
 };
