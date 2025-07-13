b0f0f82bce22a5732309c9039559c9219ef905b3
adityaexe <kmradi2002@gmail.com>
2025-07-13 08:32:08 +0530

    Add Home component with Navbar, Hero, and About sections

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import About from "../components/About";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
+};
+
+export default Home;
