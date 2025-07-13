2b016aa625f20fb6a21b32514214084ad347ac64
adityaexe <kmradi2002@gmail.com>
2025-07-13 08:48:04 +0530

    Add Faq component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import About from "../components/About";
 import Hero from "../components/Hero";
+import Faq from "../components/Faq";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Hero />
       <About />
+      <Faq />
     </div>
   );
 };
