8c716c95d9c38f759f67b16c16d7c1467b58bf76
adityaexe <kmradi2002@gmail.com>
2025-07-15 10:34:26 +0530

    remove consol.log api key

--- src/config/grmini.js
+++ src/config/grmini.js
@@ -5,7 +5,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
-console.log("API Key:", apiKey);
+
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
